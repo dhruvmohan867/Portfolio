82b045c0796d6ecb52a004d4146a0150f04cda77
dhruvmohan867 <dhruvmohanshukla@gmail.com>
2024-08-05 00:29:40 +0530

    Changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,7 +53,7 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
+                <img className="isVisible" src={contactImg} alt="Contact Us" />
               }
             </TrackVisibility>
           </Col>
