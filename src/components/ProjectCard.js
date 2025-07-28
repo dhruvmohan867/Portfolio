72e3cdf8459ea00831d8ed29f089d902d053df92
Dhruv Mohan Shukla <127665916+dhruvmohan867@users.noreply.github.com>
2025-07-29 00:20:23 +0530

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
-    <div className="proj-imgbx">
-      <img src={imgUrl} alt={title} />
+    <div className="proj-imgbx card-equal-height">
+      <img src={imgUrl} alt={title} className="project-image" />
       <div className="proj-txtx">
         <h4>{title}</h4>
         <span>{description}</span>
       </div>
-      {/* <div className="project-link-container">
-        <a href={url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">View Project</a>
-      </div> */}
     </div>
-  )
-}
+  );
+};
+
